a32280dfa7b1fda019b36602775108973368d330
JiayiZhou <jiayizhou@JiayiZhous-MacBook-Pro.local>
2023-09-12 22:14:57 -0400

    changed

--- main.py
+++ main.py
@@ -3,20 +3,44 @@ import polars as pl
 import matplotlib.pyplot as plt
 
 
+def read_file(input1):
+    """Read a file"""
+    df1 = pl.read_csv(input1)
+    return df1
+
+
 def describe_file(input1):
-    """define a function read a file"""
+    """Describe the data frame"""
     df1 = pl.read_csv(input1)
     summary_statistics = df1.describe()
     return summary_statistics
 
 
 def plot_file(input1):
-    """define a function plot relationship in a dataframe"""
+    """Plot relationship in a dataframe"""
     df1 = pl.read_csv(input1)
     plt.scatter(df1["year"], df1["gwar"])
     plt.title("Start year of season vs Goose Wins Above Replacement")
     plt.xlabel("Start year of season")
     plt.ylabel("Goose Wins Above Replacement")
-    plt.show()
     # Save the plot as a figure
     plt.savefig("plant_capacity_factor.png")
+
+
+def main():
+    """Main function to run the functions"""
+    data1 = "https://raw.githubusercontent.com/fivethirtyeight/data/master/goose/goose_rawdata.csv"
+    # read dataset
+    df1 = read_file(data1)
+    print(df1)
+
+    # compute summary statistics
+    df2 = describe_file(data1)
+    print(df2)
+
+    # generate plot
+    plot_file(data1)
+
+
+if __name__ == "__main__":
+    main()
--- test_main.py
+++ test_main.py
@@ -1,12 +1,20 @@
 """
 Test Cases
 """
-from main import describe_file
-from main import plot_file
+import polars as pl
+from main import read_file, describe_file, plot_file
 
 
 def test_read():
-    """test function"""
+    """test function for read"""
+    df1 = read_file(
+        "https://raw.githubusercontent.com/fivethirtyeight/data/master/goose/goose_rawdata.csv"
+    )
+    assert isinstance(df1, pl.DataFrame), "Fail to read the csv file"
+
+
+def test_describe():
+    """test function for describe dataframe"""
     # read a csv file by url
     df1 = describe_file(
         "https://raw.githubusercontent.com/fivethirtyeight/data/master/goose/goose_rawdata.csv"
@@ -14,10 +22,22 @@ def test_read():
     assert df1[["describe", "year"]][4, 1] == 1921.0
     assert df1[["describe", "ppf"]][0, 1] == 30962.0
     assert df1[["describe", "ppf"]][4, 1] == 88.0
+
+
+def test_plot():
+    """test function for plot"""
     plot_file(
         "https://raw.githubusercontent.com/fivethirtyeight/data/master/goose/goose_rawdata.csv"
     )
+    assert True, "Plot generation successed"
+
+
+def test_main():
+    """test main function"""
+    test_read()
+    test_describe()
+    test_plot()
 
 
 if __name__ == "__main__":
-    test_read()
+    test_main()
