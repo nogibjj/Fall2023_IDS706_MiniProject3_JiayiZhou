26dd722e754bff697110af1c0f61f3f886c4cd87
JiayiZhou36 <143651921+JiayiZhou36@users.noreply.github.com>
2023-09-12 20:33:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 
 def describe_file(input1):
     """define a function read a file"""
-    df1 = pl.read_csv(input1)
+    df1 = pl.read_csv(input1, parse_dates=False)
     summary_statistics= df1.describe()
     summary_html = summary_statistics.to_html("summary_statistics.html")
     return summary_statistics, summary_html
 
 def plot_file(input1):
     """define a function plot relationship in a dataframe"""
-    df1 = pl.read_csv(input1)
+    df1 = pl.read_csv(input1, parse_dates=False)
     df1.plot(x='CAPFAC', y='PLNGENAN', kind='scatter', 
              title="Plant Capacity Factor vs Plant Annual Net Generation")
     # Save the plot as a figure
