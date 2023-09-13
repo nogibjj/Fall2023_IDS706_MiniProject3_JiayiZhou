104ee92b63a06c5b976edf2787656718b4db4c9c
JiayiZhou36 <143651921+JiayiZhou36@users.noreply.github.com>
2023-09-12 20:15:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 """Main function goes here"""
-import pandas as pd
+import polars as pl
 import matplotlib.pyplot as plt
 
 def describe_file(input1):
     """define a function read a file"""
-    df1 = pd.read_csv(input1)
+    df1 = pl.read_csv(input1)
     summary_statistics= df1.describe()
     summary_html = summary_statistics.to_html("summary_statistics.html")
     return summary_statistics, summary_html
 
 def plot_file(input1):
     """define a function plot relationship in a dataframe"""
-    df1 = pd.read_csv(input1)
+    df1 = pl.read_csv(input1)
     df1.plot(x='CAPFAC', y='PLNGENAN', kind='scatter', 
              title="Plant Capacity Factor vs Plant Annual Net Generation")
     # Save the plot as a figure
