fcca9e8fe342e9a6f664ff3c7f63fae56f27dfa4
JiayiZhou36 <143651921+JiayiZhou36@users.noreply.github.com>
2023-09-12 20:27:55 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -12,7 +12,7 @@ def test_read():
     assert df1["SEQPLT16"]["mean"] == 4855.0
     assert df1["SEQPLT16"]["count"] == 9709.0
     assert df1["CAPFAC"]["count"] == 8038.0
-    plot_file("https://www.dropbox.com/s/x2awp0e9znsyub7/egrid2016.csv?dl=1")
+    plot_file("egrid2016.csv")
 
 if __name__ == "__main__":
     test_read()
