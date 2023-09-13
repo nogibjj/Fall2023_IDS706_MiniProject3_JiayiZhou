0f6ef27b87679939fa7af0b96ff95b5a56341cf4
JiayiZhou36 <143651921+JiayiZhou36@users.noreply.github.com>
2023-09-12 20:25:02 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -8,7 +8,7 @@ from main import plot_file
 def test_read():
     """test function"""
     #read a csv file by url
-    df1 = describe_file("https://www.dropbox.com/s/x2awp0e9znsyub7/egrid2016.csv?dl=1")[0]
+    df1 = describe_file("egrid2016.csv")[0]
     assert df1["SEQPLT16"]["mean"] == 4855.0
     assert df1["SEQPLT16"]["count"] == 9709.0
     assert df1["CAPFAC"]["count"] == 8038.0
